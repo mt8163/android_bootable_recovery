cd060057127cefaec30fe728198246e3f9ee962e
Joel Stein <js@funkturm.net>
2019-03-20 02:09:55 +0100

    Make hacked_bl_button selectable without enabling amonet

--- Android.mk
+++ Android.mk
@@ -294,6 +294,9 @@ endif
 ifeq ($(TW_AMONET), true)
     LOCAL_CFLAGS += -DTW_AMONET
 endif
+ifeq ($(TW_HACKED_BL_BUTTON), true)
+    LOCAL_CFLAGS += -DTW_HACKED_BL_BUTTON
+endif
 ifeq ($(TW_NO_REBOOT_RECOVERY), true)
     LOCAL_CFLAGS += -DTW_NO_REBOOT_RECOVERY
 endif
